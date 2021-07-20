f161ec476f27a8c0304ace1db2399ac7479dedf3
Kristopher Takken <63102467+kjtakke@users.noreply.github.com>
2021-07-20 18:36:35 +1000

    Update StringToDF.py

--- StringToDF.py
+++ StringToDF.py
@@ -2,15 +2,15 @@ def arrayFromDelToDF(string, cl, ln, cols):
     import pandas as pd
     import numpy as np
     """
-    String: To be turned into an array then converted onto a dataframe
+    string: To be turned into an array then converted onto a dataframe
     cl: Column Deliminator
     ln: Line break deliminator
-    Cols: An array or column names
+    cols: An array or column names
     
     Example:
     string = "a{%,%}b{%;%}a{%,%}b{%;%}a{%,%}b{%;%}a{%,%}b{%;%}a{%,%}b"
-    Output = arrayFromDelToDF(j, "{%,%}", "{%;%}",['Col1','Col2'])
-    
+    Input = arrayFromDelToDF(string, "{%,%}", "{%;%}",['Col1','Col2'])
+    Output:
       Col1 Col2
     0	a	b
     1	a	b
@@ -26,12 +26,17 @@ def arrayFromDelToDF(string, cl, ln, cols):
         
     return pd.DataFrame(lg, columns=cols)
 
+#Table 1
 j = "a{%,%}b{%;%}c{%,%}b{%;%}a{%,%}b{%;%}a{%,%}b{%;%}d{%,%}b{%;%}a{%,%}b{%;%}a{%,%}b{%;%}a{%,%}b{%;%}a{%,%}b{%;%}a{%,%}b{%;%}a{%,%}b{%;%}a{%,%}b"
 js = arrayFromDelToDF(j, "{%,%}", "{%;%}",['Column 1','Column 2'])
 
-
+#Table 2
 l = "a{%,%}b{%,%}c{%,%}d{%;%}c{%,%}b{%,%}c{%,%}d"
 ls = arrayFromDelToDF(l, "{%,%}", "{%;%}",['Column 3','Column 4','Column 5','Column 6'])
+
+#Join
 df = pd.merge(js, ls, left_on=['Column 1','Column 2'], right_on=['Column 3','Column 4'], how='left')
+
+#Drop Columns
 df = df.drop(['Column 3','Column 4'], 1)
 df.head()
